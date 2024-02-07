8986f74372e491dc6e886e5125e8f22e083dd457
a-gafron <gafron-alex@gmx.de>
2024-02-07 21:51:33 +0100

    Defined Method grainsOnBoard

--- Grains.java
+++ Grains.java
@@ -25,4 +25,21 @@ public class Grains {
         return squareBoard[squareBoard.length-1][1];
 
     }
+
+    public static BigInteger grainsOnBoard () {
+        BigInteger[][] squareBoard = new BigInteger[64][3];
+
+        for (int i = 0; i < squareBoard.length; i++) {
+            squareBoard[i][0] = BigInteger.valueOf((i+1));
+        }
+
+        for (int i = 0; i < squareBoard.length; i++){
+            squareBoard[i][1] = BigInteger.valueOf(2).pow(i);
+        }
+        for (int i = 0; i < 64; i++){
+            squareBoard[i][2] = squareBoard[i][1].multiply(BigInteger.valueOf(2)).subtract(BigInteger.ONE);
+        }
+
+        return squareBoard[63][2];
+    }
 }
