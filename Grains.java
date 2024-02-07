0520d95e14b1c23ae49e3ac6774174d1bfa9ebee
a-gafron <gafron-alex@gmx.de>
2024-02-07 21:50:18 +0100

    Defined Method testArgument

--- Grains.java
+++ Grains.java
@@ -1,3 +1,9 @@
 public class Grains {
 
+    public static void testArgument (int testSquare) {
+        if (testSquare < 1 || testSquare > 64) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+    }
+
 }
