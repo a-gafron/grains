62ad83a52ca65acba9d93df3c1142383bcd9cc80
a-gafron <gafron-alex@gmx.de>
2024-02-07 21:50:52 +0100

    Defined Method grainsOnSquare

--- Grains.java
+++ Grains.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class Grains {
 
     public static void testArgument (int testSquare) {
@@ -6,4 +8,21 @@ public class Grains {
         }
     }
 
+    public static BigInteger grainsOnSquare (int square) {
+        testArgument(square);
+
+        BigInteger [][] squareBoard = new BigInteger[square][2];
+
+        for (int i = 0; i < squareBoard.length; i++) {
+            squareBoard[i][0] = BigInteger.valueOf((i+1));
+        }
+        squareBoard[0][1] = BigInteger.valueOf(1);
+
+        for (int i = 0; i < squareBoard.length; i++){
+            squareBoard[i][1] = BigInteger.valueOf(2).pow(i);
+        }
+
+        return squareBoard[squareBoard.length-1][1];
+
+    }
 }
